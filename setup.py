d02d1d29b1378bc9b6753eb37cfac821caf08f04
miko <n.mikolaj@gmail.com>
2015-02-11 14:57:12 +0100

    PyPI

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+  name = 'json-digger',
+  packages = ['json-digger'],
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Mikolaj Nowak',
+  author_email = 'mikolaj.nowak@likedata.pl',
+  url = 'https://github.com/likedata/py-json-digger',
+  download_url = 'https://github.com/likedata/py-json-digger/tarball/0.1',
+  keywords = ['python', 'json', 'digger', 'dict', 'nested', 'py', 'dictionary', 'multi-tier'],
+  classifiers = [],
+)
