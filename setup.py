270fb96623c0ac9b27c8c39ba1f4814c6abdf337
miko <n.mikolaj@gmail.com>
2015-02-11 16:35:25 +0100

    PyPI

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@
 from distutils.core import setup
 
 setup(
-  name='json-digger',
-  packages=['json_digger'],
-  version='0.1',
-  description='The script is to help you look either for specific keys or values in multi-tier nested python dictionaries (either native or converted from JSON).',
-  author='Mikolaj Nowak',
-  author_email='mikolaj.nowak@likedata.pl',
-  license='MIT',
-  url='https://github.com/likedata/py-json-digger',
-  download_url='https://github.com/likedata/py-json-digger/tarball/0.1',
-  keywords=['python', 'json', 'digger', 'dict', 'nested', 'py', 'dictionary', 'multi-tier'],
-  classifiers=[],
-  )
+  name = 'json-digger',
+  packages = ['json_digger'],
+  version = '0.1',
+  description = 'The script is to help you look either for specific keys or values in multi-tier nested python dictionaries (either native or converted from JSON).',
+  author = 'Mikolaj Nowak',
+  author_email = 'mikolaj.nowak@likedata.pl',
+  license = 'MIT',
+  url = 'https://github.com/likedata/py-json-digger',
+  download_url = 'https://github.com/likedata/py-json-digger/tarball/0.1',
+  keywords = ['python', 'json', 'digger', 'dict', 'nested', 'py', 'dictionary', 'multi-tier'],
+  classifiers = [],
+)
